97ec27602852de9cd1f0968f53d11bb5fddb0d61
alireza <mehdimj0161@gmail.com>
2021-12-09 21:24:50 +0330

    created header slider progress

--- js/home.js
+++ js/home.js
@@ -31,33 +31,69 @@ function headerSlider(){
     // access to slider navigation buttons
     const nextBtn = document.querySelector(".next-slide");
     const prevBtn = document.querySelector(".prev-slide");
+    // access to the slider progress
+    const sliderProgress = document.querySelector('.slider-progress');
+    const progress = sliderProgress.firstElementChild;
     // created counter 
     let i = 0;
+
+    // access to progress heigth
+    const progressHeight = 100 / slides.length;
+    // set progress heigth 
+    progress.style.height = progressHeight + '%';
+    // created progress position
+    let progressPosition = 0;
+
+    // next slide button
     nextBtn.addEventListener('click', (e) => {
+        // transform slider progress
+        progressPosition += progressHeight;
+        progress.style.top = `${progressPosition}%`
+
+        // hide this slide
         slides[i].classList.remove('active');
+        // show next slide
         slides[i + 1].classList.add('active');
+        // active prevSlide button
         prevBtn.style.pointerEvents = 'painted';
         prevBtn.style.opacity = 1;
+        // counter + 1
         i++;
         
+        // disabled next btn
         if(i == slides.length - 1){
             nextBtn.style.pointerEvents = 'none';
             nextBtn.style.opacity = .5;
         }
-        console.log(i);
     });
-    
+    // prev slide button
     prevBtn.addEventListener('click', (e) => {
+        // transform slider progress
+        progressPosition -= progressHeight;
+        progress.style.top = `${progressPosition}%`
+        
+        // hide this slide
         slides[i].classList.remove('active');
+        // show prev slide
         slides[i - 1].classList.add('active');
+        // active next slide btn
         nextBtn.style.pointerEvents = 'painted';
         nextBtn.style.opacity = 1;
+        // counter - 1
         i--;
         
+        // disabled prev button
         if(i == 0){
             prevBtn.style.pointerEvents = 'none';
             prevBtn.style.opacity = .5;
         }
-        console.log(i);
     });
+    
+
+    // access to end count from scroll progress
+    const endCount = document.querySelector('#end-count');
+    
+    // set slides length to end counter
+    endCount.innerHTML += slides.length;
+
 }
